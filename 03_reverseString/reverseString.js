340d37273628e190c6908842efd21e48a011cdd8
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-09 13:02:24 +0300

    test 3 passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const splitLetters = string.split("");
+  console.log(splitLetters);
+  splitLetters.reverse();
+  console.log(splitLetters);
+  const joinedLetters = splitLetters.join("");
+  console.log(joinedLetters);
+  return joinedLetters;
 };
 
 // Do not edit below this line

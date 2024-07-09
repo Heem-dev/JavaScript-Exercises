8e538eb822b8c1a0fa27a3070193637b20d8a0f9
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-09 18:06:18 +0300

    test 5 passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,22 @@
 const sumAll = function (num1, num2) {
   let numAll = 0;
-
-  for (i = num2; i <= num2 && i >= num1; i--) {
-    console.log(i);
-    console.log(numAll);
-    numAll += i;
+  const highestNum = Math.max(num1, num2);
+  const lowestNum = Math.min(num1, num2);
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 != "number" ||
+    typeof num2 != "number"
+  ) {
+    return "ERROR";
+  } else {
+    for (i = highestNum; i <= highestNum && i >= lowestNum; i--) {
+      // console.log(i);
+      // console.log(numAll);
+      numAll += i;
+    }
+    return numAll;
   }
-  return numAll;
 };
-
 // Do not edit below this line
 module.exports = sumAll;

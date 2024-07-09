0fa3e20aa8f2f8876b87cf107ca545f62b84b72a
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-09 15:05:37 +0300

    solution too slow for large numbers. will fix it.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let numAll = 0;
 
+  for (i = num2; i <= num2 && i >= num1; i--) {
+    console.log(i);
+    console.log(numAll);
+    numAll += i;
+  }
+  return numAll;
 };
 
 // Do not edit below this line

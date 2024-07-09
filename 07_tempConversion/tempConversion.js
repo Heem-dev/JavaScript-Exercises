41433c1d1c98eceb2dc920ae57f58e4b1732ec84
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-09 20:14:47 +0300

    test 7 passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  const convered = (degree - 32) / 1.8;
+  let rounded = convered.toFixed(1);
+  rounded = parseFloat(rounded);
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  const convered = degree * 1.8 + 32;
+  let rounded = convered.toFixed(1);
+  rounded = parseFloat(rounded);
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

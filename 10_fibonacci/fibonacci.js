84e2b28513aed59c5b216d96204c9c0684392134
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-10 15:05:50 +0300

    test 10 passed.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseFloat(num);
+  const indexRequested = num;
+  // return oops if negative number
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+  let fibNums = [1, 1, 2];
 
+  // we make a loop to generate numbers needed to reach the requested index
+  // we use num>3 cuz we already have 3 numbers in the array so no need to generate unneeded numbers
+  while (num > 3) {
+    num--;
+    fibNums.push(fibNums.at(-1) + fibNums.at(-2));
+  }
+  console.log(indexRequested);
+  console.log(fibNums);
+  return fibNums[indexRequested - 1];
 };
 
 // Do not edit below this line

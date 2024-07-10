f48d263e5d87d7cac5ff0996803c5eeb954c2a4f
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-10 13:03:07 +0300

    test 8 passed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sumVal = 0;
+  array.forEach((val) => {
+    sumVal += val;
+  });
+  return sumVal;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let sumVal = 1;
+  array.forEach((val) => {
+    sumVal *= val;
+  });
+  return sumVal;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let numFactorial = num;
+  if (num == 0) return 1;
+
+  while (num > 1) {
+    num--;
+    numFactorial = numFactorial * num;
+  }
+  return numFactorial;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

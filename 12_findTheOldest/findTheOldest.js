e54878c85514fd939d084302d182152f254b3129
Heem Dev <40331543+Heem-dev@users.noreply.github.com>
2024-07-10 18:47:44 +0300

    test 12 passed.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  // add age property to each person even if there is no death year
+  people.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.age = new Date().getFullYear() - person.yearOfBirth;
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+  // make an array of people sorted from oldest to youngest
+  let sortedPeople = people.toSorted(
+    (person1, nextPerson) => nextPerson.age - person1.age
+  );
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
